1da2578def77b84c3900c36677f83ea1acd09600
jeffr <jeffr@DELL-JR4518>
2024-10-07 22:28:52 -0400

    Add project files.

---
+++ twainwpf/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////
+//
+// TWAINWPFScan.ScanWindow
+//
+// This class is the WPF version of TWAINCSScan.FormScan.
+//
+// Note: This project is intended for practical use in WPF and the software is provided
+// "AS IS". Performance on scanning and compatibility with different operating systems 
+// or environments is not guaranteed. Users are advised to test the software in 
+// their own environments to ensure compatibility.
+//
+///////////////////////////////////////////////////////////////////////////////////////
+//  Author             Date            Version     Comment
+//  C H Jeffrey Tong
+//
+///////////////////////////////////////////////////////////////////////////////////////
+//
+// TWAINCSScan.FormScan
+//
+// This is the main class for the application.  We're showing how to select and
+// load a TWAIN driver.  How to configure it for a scan session, and how to capture
+// and display images.
+//
+// This (moreso than then TWAINCSTst) is designed to be a possible template for
+// developers looking to add TWAIN to their C# applications.  It's small, and it's
+// focused on the tasks needed to capture image data.
+//
+///////////////////////////////////////////////////////////////////////////////////////
+//  Author          Date            Version     Comment
+//  M.McLaughlin    21-May-2014     2.0.0.0     64-bit Linux
+//  M.McLaughlin    27-Feb-2014     1.1.0.0     ShowImage additions
+//  M.McLaughlin    21-Oct-2013     1.0.0.0     Initial Release
+///////////////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) 2013-2019 Kodak Alaris Inc.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+///////////////////////////////////////////////////////////////////////////////////////
+
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace twainwpfscan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
